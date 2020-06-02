82e3ca099635633f27cafdccbf5b32deec05f2aa
mrZaklik <4061988@gmail.com>
2020-06-02 18:46:21 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,123 @@
+console.log ('!! TASK-1');
+for (let i11 =1; i11 <= 50; i11++) {
+    console.log (i11);
+}
+
+console.log ('--И--');
+
+for (let i12 =35; i12 >= 8; i12--) {
+    console.log (i12);
+}
+
+
+console.log ('!! TASK-2');
+let i2 = 89;
+while (i2 >= 11) {
+    console.log (i2);
+    i2--;
+}
+// зачем там <br /> я не понимаю, итак всё в столбик, и как его туда всунуть тоже не понимаю.
+
+
+console.log ('!! TASK-3');
+for (let i3 = 0; i3 <= 100; i3++) {
+    s3 = 0;
+    for (let j3 = 0; j3 <= i3; j3++) {
+        s3 += j3;
+    }
+    console.log ('sum ' + i3 + ' = ' + s3);
+}
+
+
+console.log ('!! TASK-4');
+for (let i4 = 1; i4 <= 5; i4++) {
+    s4 = 0;
+    for (let j4 = 1; j4 <= i4; j4++) {
+        s4 += j4;
+    }
+    console.log ('sum ' + i4 + ' = ' + s4);
+}
+
+
+console.log ('!! TASK-5');
+for (let i51 =8; i51 <= 56; i51+=2) {
+     console.log (i51);
+}
+
+console.log ('--И--');
+
+let i52 = 8;
+while (i52 <= 56) {
+    console.log (i52);
+    i52+=2;
+}
+
+
+console.log ('!! TASK-6');
+
+console.log ('--2*N--');
+let j62 = 2;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j62;
+    console.log (j62 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--3*N--');
+let j63 = 3;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j63;
+    console.log (j63 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--4*N--');
+let j64 = 4;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j64;
+    console.log (j64 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--5*N--');
+let j65 = 5;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j65;
+    console.log (j65 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--6*N--');
+let j66 = 6;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j66;
+    console.log (j66 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--7*N--');
+let j67 = 7;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j67;
+    console.log (j67 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--8*N--');
+let j68 = 8;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j68;
+    console.log (j68 + ' * ' + i6 + ' = ' + s6);
+}
+
+console.log ('--9*N--');
+let j69 = 9;
+for (let i6 = 1; i6 <= 10; i6++) {
+    s6 = 0;
+        s6 += i6 * j69;
+    console.log (j69 + ' * ' + i6 + ' = ' + s6);
+}
+// как сделать j автоматической я не понял, если опять же это надо было делать и мы вообще знаем это.
+
+// 7, 8, 9, 10 задание я не понимаю как делать, у меня впечатление, что не хватает видео уроков по этим темам.
